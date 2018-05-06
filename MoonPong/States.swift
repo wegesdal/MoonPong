08fc1567bc9a218fb713e8b90172211b8b50ad92
William Egesdal <william.egesdal@gmail.com>
2018-05-06 10:24:24 -1000

    tracer 1.0 working

--- MoonPong/States.swift
+++ MoonPong/States.swift
@@ -9,25 +9,6 @@
 import Foundation
 import GameplayKit
 
-class SquaringUp: GKState {
-    
-    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
-        return stateClass is IncomingBall.Type
-    }
-    
-    override func didEnter(from previousState: GKState?) {
-        print("Opponent is squaring up.")
-    }
-    
-    override func update(deltaTime: TimeInterval) {
-        GameViewController.player2.position.y = GameViewController.player1.position.y
-        GameViewController.player2.position.x = GameViewController.player1.position.x * -1
-        GameViewController.player2.position.z = GameViewController.player1.position.z * -1
-        
-    }
-    
-}
-
 class IncomingBall: GKState {
     static var t: TimeInterval = 0
     override func isValidNextState(_ stateClass: AnyClass) -> Bool {
@@ -48,13 +29,13 @@ class Striking: GKState {
     static let flashInterval = 0.2
     static var moved: Bool = false
     override func isValidNextState(_ stateClass: AnyClass) -> Bool {
-        return stateClass is SquaringUp.Type
+        return stateClass is IncomingBall.Type
     }
     
     override func didEnter(from previousState: GKState?) {
         print("Opponents strikes the ball.")
         Striking.moved = false
-        GameViewController.paddle2.position.y = 0
+//        GameViewController.paddle2.position.y = 0
     }
     
     override func update(deltaTime: TimeInterval) {
